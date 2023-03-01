4285761acd141090f9bdf473d2f6a490fd4fa475
Ямшанов Константин Алексеевич (4059153) <yamshanov@dev.vtb.ru>
2023-03-01 22:38:52 +0400

    Разработка от 01.03.2023. Оптимизация конфигурации монги Фикс создания индексов

--- src/main/kotlin/ru/kyamshanov/mission/profile/MissionProfileApplication.kt
+++ src/main/kotlin/ru/kyamshanov/mission/profile/MissionProfileApplication.kt
@@ -1,9 +1,10 @@
 package ru.kyamshanov.mission.profile
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [MongoReactiveAutoConfiguration::class])
 class MissionProfileApplication
 
 fun main(args: Array<String>) {
